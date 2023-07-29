8f312d14e80049659b0f73980aa95a639ea0193e
MrSurenK <surenkworkz@gmail.com>
2023-07-29 17:47:06 +0800

    Setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import CoinPage from "./pages/CoinPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
