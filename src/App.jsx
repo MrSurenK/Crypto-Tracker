0a0511f9e47fa0e8d858a4eb9e528bce8e7c671c
MrSurenK <surenkworkz@gmail.com>
2023-07-30 03:13:28 +0800

    Made the NavBar with logo and currency toggle

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import "./App.css";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 import CoinPage from "./pages/CoinPage";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      {/* Sets the background color for the entire website using Box component in MUI */}
+      <Box sx={{ bgcolor: "#001B35", color: "white", minHeight: "100vh" }}>
         <Header />
         <Routes>
           <Route index element={<Homepage />} />
           <Route path="/coin/:id" element={<CoinPage />} />
         </Routes>
-      </div>
+      </Box>
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { AppBar, Select, MenuItem } from "@mui/material";
+import { Container } from "@mui/system";
+import { Toolbar, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const NavBar = () => {
-  return <div>Header</div>;
+// todo: Context API to keep it constant
+// todo: fix value error for currency toggle
+
+const Header = () => {
+  // This hook is to return to the homepage whenever the crypto Tracker logo is cliked (Refer to onClick at Typography component)
+  const navigate = useNavigate();
+
+  // Defined custom dark theme for the NavBar
+  const darkMode = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      mode: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkMode}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              sx={{
+                flex: 1,
+                color: "white",
+                fontFamily: "Roboto",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+              variant={"h6"}
+              onClick={() => navigate("/")}
+            >
+              Crypto Tracker
+            </Typography>
+
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginRight: 15 }}
+            >
+              <MenuItem value={"SGD"}>SGD</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
-export default NavBar;
+export default Header;
