df22a3f524c013ab033bf65b641aa3ac1a653f91
MrSurenK <surenkworkz@gmail.com>
2023-08-01 17:29:58 +0800

    Started on coin page

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { CurrencyState } from "../CurrencyContext";
+import { SingleCoin } from "../config/endpoints";
+import { Box } from "@mui/system";
 
 const CoinPage = () => {
-  return <div>Coin Page</div>;
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency } = CurrencyState();
+
+  const fetchCoin = async () => {
+    const { data } = await fetch(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  // Call the API with useEffect
+
+  useEffect(() => {
+    fetchCoin();
+    console.log(coin);
+  }, []);
+
+  return <Box sx={{}}></Box>;
 };
 
 export default CoinPage;
