ff07ba56e776c4c2021cb82daa8fed4997f5685a
MrSurenK <surenkworkz@gmail.com>
2023-08-02 20:34:01 +0800

    imported relevant states for chart

--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CurrencyState } from "../CurrencyContext";
 
 const CoinInfo = () => {
+  // Initialise historical data state
+  const [historicalData, setHistoricalData] = useState();
+
+  // Initialise state for day at 1
+  const [days, setDays] = useState(1);
+
+  // import Context API to get currency state
+  const { currency } = CurrencyState();
+
   return <div>Coin Info</div>;
 };
 
