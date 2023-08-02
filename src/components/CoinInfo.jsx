0a3199233ca27815655144f084dba99ee2a0726c
MrSurenK <surenkworkz@gmail.com>
2023-08-02 21:05:53 +0800

    Fetched the data out of the API and changed state

--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { CurrencyState } from "../CurrencyContext";
+import { HistoricalChart } from "../config/endpoints";
 
-const CoinInfo = () => {
+const CoinInfo = ({ coin }) => {
   // Initialise historical data state
-  const [historicalData, setHistoricalData] = useState();
+  const [historicalPrice, setHistoricalPrice] = useState();
 
   // Initialise state for day at 1
   const [days, setDays] = useState(1);
@@ -11,7 +12,21 @@ const CoinInfo = () => {
   // import Context API to get currency state
   const { currency } = CurrencyState();
 
-  return <div>Coin Info</div>;
+  // call the API
+  const fetchHistoricalPrices = async () => {
+    const res = await fetch(HistoricalChart(coin.id, days, currency));
+    const data = await res.json();
+    console.log(data);
+    setHistoricalPrice(data.prices);
+  };
+
+  // Call the data with useEffect on mount
+  // Update state if currency or days changes
+  useEffect(() => {
+    fetchHistoricalPrices();
+  }, [currency, days]);
+
+  return <div>Coin Chart</div>;
 };
 
 export default CoinInfo;
