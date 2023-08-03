44940b916accc73185ee5b0cfa314378e7b59346
K.Surendraraj <surenkworkz@gmail.com>
2023-08-03 12:41:23 +0800

    Watchlist button styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AppBar, Select, MenuItem } from "@mui/material";
 import { Container } from "@mui/system";
 import { Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CurrencyState } from "../CurrencyContext";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 const Header = () => {
   // This hook is to return to the homepage whenever the crypto Tracker logo is cliked (Refer to onClick at Typography component)
@@ -12,6 +14,9 @@ const Header = () => {
 
   // Import currency state from CurrencyContext
   const { currency, setCurrency } = CurrencyState();
+
+  // Create state for Watchlist
+  const { watchList, addWatchList } = useState([]);
 
   // Defined custom dark theme for the NavBar
   const darkMode = createTheme({
@@ -41,16 +46,23 @@ const Header = () => {
             >
               CRYPTO TRACKER
             </Typography>
-
-            <Select
-              variant="outlined"
-              style={{ width: 100, height: 40, marginRight: 15 }}
-              defaultValue="SGD"
-              onChange={(e) => setCurrency(e.target.value)}
-            >
-              <MenuItem value={"SGD"}>SGD</MenuItem>
-              <MenuItem value={"USD"}>USD</MenuItem>
-            </Select>
+            <Stack spacing={2} direction="row">
+              <Button
+                variant="contained"
+                sx={{ bgcolor: "orange", color: "black" }}
+              >
+                Watchlist
+              </Button>
+              <Select
+                variant="outlined"
+                style={{ width: 100, height: 40, marginRight: 15 }}
+                defaultValue="SGD"
+                onChange={(e) => setCurrency(e.target.value)}
+              >
+                <MenuItem value={"SGD"}>SGD</MenuItem>
+                <MenuItem value={"USD"}>USD</MenuItem>
+              </Select>
+            </Stack>
           </Toolbar>
         </Container>
       </AppBar>
